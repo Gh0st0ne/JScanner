0071e9fcf7e039de4bdfab26b2a0551bd478e47b
machinexa <machinexa@gmail.com>
2020-09-27 22:31:53 +0545

    10:31:53 PM 09/27/2020

--- JScanner.py
+++ JScanner.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 from re import search
 from requests import get
 from termcolor import colored
@@ -8,74 +9,78 @@ from jsbeautifier import beautify
 from argparse import ArgumentParser
 from concurrent.futures import ThreadPoolExecutor
 
-from lib.Functions import starter
+from lib.Functions import starter, manage_output
 from lib.PathFunctions import PathFunction
 from lib.Globals import *
 
-FPathApp = PathFunction()
-
 parser = ArgumentParser(description=colored('Javascript Scanner', color='yellow'), epilog=colored("Enjoy bug hunting", color='yellow'))
-parser.add_argument('-w', '--wordlist', type=str, help='Absolute path of wordlist')
-parser.add_argument('-oD', '--output-directory', type=str, help="Output directory")
+input_group = parser.add_mutually_exclusive_group()
+output_group = parser.add_mutually_exclusive_group()
+input_group.add_argument('---', '---', action="store_true", dest="stdin", help="Stdin")
+input_group.add_argument('-w', '--wordlist', type=str, help='Absolute path of wordlist')
 parser.add_argument('-d', '--domain', type=str, help="Domain name")
+output_group.add_argument('-oD', '--output-directory', type=str, help="Output directory")
+output_group.add_argument('-o', '--output', type=str, help="Output file")
 parser.add_argument('-t', '--threads', type=int, help="Number of threads")
 parser.add_argument('-b', '--banner', action="store_true", help="Print banner and exit")
 argv = parser.parse_args()
-
-starter(argv)
+input_wordlist = starter(argv)
 
 def scan_js(url: str) -> bool:
-    global output_file
+    FPathApp = PathFunction()
+    output_list = []
     urlparser = urlparse(url)
     if search(".*\.js$", urlparser.path):
         try:
             jsurl = FPathApp.slasher(FPathApp.urler(urlparser.netloc)) + FPathApp.payloader(urlparser.path)
             print(f"{ColorObj.information} Trying to get data from {colored(jsurl, color='cyan')}")
-            output_file.write("{:<40} <--- URL\n".format(jsurl))
+            output_list.append(manage_output(f"{jsurl}  <--- URL\n"))
             jsreq = get(jsurl)
             jstext = str(beautify(jsreq.text)).split('\n')
             for jsline in jstext:
                 for dom_source in dom_sources_regex:
                     if search(dom_source, jsline):
                         print(f"{ColorObj.good} Found sensitive data: {colored(jsline, color='cyan')}")
-                        output_file.write("{:<40} <--- From regex {}\n".format(jsline.strip(' '), dom_source))
+                        output_list.append(manage_output(f"{jsline.strip(' ')} <--- From regex {dom_source}\n"))
                 for dom_sink in dom_sinks_regex:
                     if search(dom_sink, jsline):
                         print(f"{ColorObj.good} Found sensitive data: {colored(jsline, color='cyan')}")
-                        output_file.write("{:<40} <--- From regex {}\n".format(jsline.strip(' '), dom_sink))
+                        output_list.append(manage_output(f"{jsline.strip(' ')} <--- From regex {dom_sink}\n"))
             return True
         except Exception as E:
-            print(f"{ColorObj.bad} Exception {E},{E.__class__},{E.__class__} occured")
+            print(f"{ColorObj.bad} Exception {E},{E.__class__} occured")
             return False
     if not search(".*\.js$", urlparser.path):
-        jsurl = FPathApp.slasher(FPathApp.urler(urlparser.netloc)) + FPathApp.payloader(urlparser.path)
-        print(f"{ColorObj.information} Trying to get data from {colored(jsurl, color='cyan')}")
-        output_file.write("{:<40} <--- URL\n".format(jsurl))
-        jsreq = get(jsurl)
-        jsx = BeautifulSoup(jsreq.text, 'html.parser')
-        jssoup = jsx.find_all("script")
-        for jscript in jssoup:
-            if jscript != None:
-                jstext = str(beautify(jscript.string)).split('\n')
-                for jsline in jstext:
-                    for dom_source in dom_sources_regex:
-                        if search(dom_source, jsline):
-                            print(f"{ColorObj.good} Found sensitive data: {colored(jsline, color='cyan')}")
-                            output_file.write("{:<40} <--- From regex {}\n".format(jsline, dom_source))
-                    for dom_sink in dom_sinks_regex:
-                        if search(dom_sink, jsline):
-                            print(f"{ColorObj.good} Found sensitive data: {colored(jsline, color='cyan')}")
-                            output_file.write("{:<40} <--- From regex {}\n".format(jsline, dom_sink))
+        try:
+            jsurl = FPathApp.slasher(FPathApp.urler(urlparser.netloc)) + FPathApp.payloader(urlparser.path)
+            print(f"{ColorObj.information} Trying to get data from {colored(jsurl, color='cyan')}")
+            output_file.write(manage_output(f"{jsurl} <--- URL\n"))
+            jsreq = get(jsurl)
+            jsx = BeautifulSoup(jsreq.text, 'html.parser')
+            jssoup = jsx.find_all("script")
+            for jscript in jssoup:
+                if jscript != None:
+                    jstext = str(beautify(jscript.string)).split('\n')
+                    for jsline in jstext:
+                        for dom_source in dom_sources_regex:
+                            if search(dom_source, jsline):
+                                print(f"{ColorObj.good} Found sensitive data: {colored(jsline, color='cyan')}")
+                                output_list.append(manage_output(f"{jsline} <--- From regex {dom_source}\n"))
+                        for dom_sink in dom_sinks_regex:
+                            if search(dom_sink, jsline):
+                                print(f"{ColorObj.good} Found sensitive data: {colored(jsline, color='cyan')}")
+                                output_list.append(manage_output(f"{jsline} <--- From regex {dom_sink}\n"))
+            return True
+        except Exception as E:
+            print(f"{ColorObj.bad} Exception {E},{E.__class__} occured")
+            return False
+try:
+    def main():
+        global input_wordlist
+        with ThreadPoolExecutor(max_workers=argv.threads) as submitter:
+            submitter.submit(scan_js, input_wordlist)
 
-def main():
-    global output_file
-    if not argv.wordlist or not argv.output_directory or not argv.domain:
-        print(f"{ColorObj.bad} Use --help")
-        exit(0)
-    input_wordlist = [line.rstrip('\n') for line in open(argv.wordlist)]
-    output_file = open(FPathApp.slasher(argv.output_directory) + argv.domain + '.jscan', 'a')
-    
-    with ThreadPoolExecutor(max_workers=argv.threads) as mapper:
-        mapper.map(scan_js, input_wordlist)
-
-main()
+    if __name__ == "__main__":
+        main()
+except Exception as E:
+    print(E,E.__class__)
--- lib/Functions.py
+++ lib/Functions.py
@@ -1,15 +1,47 @@
+from sys import stdin
 from termcolor import colored
 from lib.Globals import ColorObj
 
 def banner():
-    from pyfiglet import print_figlet as puff
-    puff('JScanner', font='larry3d', colors='BLUE')
-    print(colored('JScanner: Tool to find javascript for secrets and vulnerabilites!', color='red', attrs=['bold']))
+    from pyfiglet import print_figlet
+    print_figlet('JScanner', font='larry3d', colors='BLUE')
+    print(colored('JScanner: Find secrets and vulnerabilites!', color='red', attrs=['bold']))
 
 def starter(argv):
     if argv.banner:
         banner()
         exit(0)
-    if not argv.wordlist or not argv.domain or not argv.output_directory or not argv.threads:
-        print("{} Use --help".format(ColorObj.bad))
-        exit()
+    if argv.output_directory:
+        if not argv.domain:
+            print(f"{ColorObj.bad} Output directory provided but not domain")
+    if not argv.wordlist:
+        if not argv.domain:
+            if not argv.stdin:
+                print(f"{ColorObj.bad} Use --help")
+                exit()
+            else:
+                stdinarray = stdin.read().split('\n')
+                return [line.rstrip('\n').strip(' ') for line in stdinarray if line]
+        else:
+            return [argv.domain]
+    else:
+        return [line.rstrip('\n').strip(' ') for line in open(argv.wordlist) if line]
+
+def output_writer(filepath, filename, to_write):
+    output_file = open(FPathApp.slasher(argv.output_directory) + argv.domain + '.jscan', 'a')
+    for jsresult in to_write:
+        output_file.write(jsresult)
+    output_file.close()
+
+def output_writer(filename, to_write):
+    output_file = open(filename, 'a')
+    for jsresult in to_write:
+        output_file.write(jsresult)
+    output_file.close()
+
+def manage_output(line) -> tuple:
+    text, appendtext = line.split('<---')
+    appendtext = '<---' + appendtext
+    appendtext = appendtext.rjust(148-len(text))
+    return str(text + appendtext)
+
