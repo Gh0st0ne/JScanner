f72286eddcc287eeb2e1de8baea06876e8d084bc
machinexa <machinexa@gmail.com>
2020-10-20 23:33:43 +0545

    11:33:43 PM 10/20/2020

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # ./run.sh | wc -l should give 35 for now
 (cd /root/MachineYadav/My-Tools/JScanner/tests; fuser -k 80/tcp 1>/dev/null 2>/dev/null; php -S 0.0.0.0:80 1>/dev/null 2>/dev/null &)
 (cd /root/MachineYadav/My-Tools/JScanner/tests; JScanner -u http://localhost/example.js -o output/done1.txt -e)
